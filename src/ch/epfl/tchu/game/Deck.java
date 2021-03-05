1e2fc4e0f2bf0b8c0f3f31a485a2ec4a886109a0
MalloryHenriet <henrietma@gmail.com>
2021-03-05 16:39:21 +0100

    Début étape 3
    Signed-off-by: Louis Dous Hervé <louisdousherve@gmail.com>

---
+++ src/ch/epfl/tchu/game/Deck.java
@@ -0,0 +1,15 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.SortedBag;
+
+import java.util.Random;
+
+public final class Deck<C extends Comparable<C>> {
+    
+    private final int size;
+    
+    public static <C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng) {
+        
+    }
+    
+}
