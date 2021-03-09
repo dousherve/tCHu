dd362afdb1fb18080e8a7542e865d09f198e9c64
MalloryHenriet <henrietma@gmail.com>
2021-03-09 22:57:11 +0100

    JaveDoc dans Deck et PublicCardState

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -7,10 +7,32 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Classe finale et immuable qui représente un tas de cartes.
+ * Et cela pour tout type de cartes comme des wagons, des locomotives ou des billets.
+ *
+ * Elle est générique car le type de cartes n'est pas fixé à l'avance
+ *
+ * @author Mallory Henriet (311258)
+ * @author Louis Hervé (312937)
+ */
 public final class Deck<C extends Comparable<C>> {
 
     private final List<C> cards;
 
+    /**
+     * Retourne un tas de cartes ayant les mêmes cartes que le multiensemnle donné.
+     * Mais les cartes sont mélangées au moyen du nombre aléatoires rng.
+     *
+     * @param cards
+     *          Multiensemble de cartes donné
+     * @param rng
+     *          Nombre aléatoire pour le mélange
+     * @param <C>
+     *          Type des cartes
+     * @return
+     *          un tas de cartes ayant les mêmes cartes que le multiensemble mais mélangées
+     */
     public static <C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng) {
         List<C> toBeShuffled = cards.toList();
         Collections.shuffle(toBeShuffled, rng);
@@ -22,29 +44,79 @@ public final class Deck<C extends Comparable<C>> {
         this.cards = Collections.unmodifiableList(cards);
     }
 
+    /**
+     * Retourne la taille du tas, c'est à dire le nombre de cartes contenues.
+     *
+     * @return
+     *          la taille du tas
+     */
     public int size() {
         return cards.size();
     }
 
+    /**
+     * Retourne vrai si et seulement si le tas est vide.
+     *
+     * @return
+     *          vrai si le tas est vide
+     *          faux dans le cas contraire
+     */
     public boolean isEmpty() {
         return cards.isEmpty();
     }
 
+    /**
+     * Retourne la carte au sommet du tas.
+     * Dans notre implémentation le sommet du tas se situe à la fin de la liste.
+     *
+     * @throws IllegalArgumentException
+     *          si le tas de carte est vide
+     * @return
+     *          la carte au sommet du tas
+     */
     public C topCard() {
         Preconditions.checkArgument(! isEmpty());
         return cards.get(size() - 1);
     }
 
+    /**
+     * Retourne un tas identique au récepteur mais sans la carte au sommet.
+     *
+     * @throws IllegalArgumentException
+     *          si le tas de carte est vide
+     * @return
+     *          un tas identique au récepteur mais sans la carte au sommet
+     */
     public Deck<C> withoutTopCard() {
         Preconditions.checkArgument(! isEmpty());
         return new Deck<>(cards.subList(0, size() - 1));
     }
 
+    /**
+     * Retourne un multiensemble contenant les <code>count</code> cartes se trouvant au sommet du tas.
+     *
+     * @param count
+     *          le nombre des cartes du sommet du tas à prendre
+     * @throws IllegalArgumentException
+     *          si <code>count</code> n'est pas compris entre 0 (inclus) et la taille du tas (incluse)
+     * @return
+     *          un multiensemble des <code>count</code> cartes au sommet du tas.
+     */
     public SortedBag<C> topCards(int count) {
         Preconditions.checkArgument(count >= 0 && count <= size());
         return SortedBag.of(cards.subList(size() - count, size()));
     }
 
+    /**
+     * Retourne un tas identique au récepteur mais sans les <code>count</code> cartes du sommet.
+     *
+     * @param count
+     *          le nombre de cartes du sommet du tas à enlever
+     * @throws IllegalArgumentException
+     *          si <code>count</code> n'est pas compris entre 0 (inclus) et la taille du tas (incluse)
+     * @return
+     *          un tas identique au récepteur mais sans les <code>count</code> cartes du sommet
+     */
     public Deck<C> withoutTopCards(int count) {
         Preconditions.checkArgument(count >= 0 && count <= size());
         return new Deck<>(cards.subList(0, size() - count));
--- src/ch/epfl/tchu/game/PublicCardState.java
+++ src/ch/epfl/tchu/game/PublicCardState.java
@@ -6,11 +6,39 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Classe publique et immuable qui représente (une partie de) l'état des cartes
+ * wagons/locomotive qui ne sont pas en main des joueurs.
+ *
+ * C'est à dire les 5 cartes disposées face visible à côté du plateau,
+ * la pioche,
+ * la défausse.
+ *
+ * Il s'agit de la partie de l'état des cartes connue de tous les joueurs.
+ * Par exemple la taille de la pioche, mais pas son contenu exact.
+ *
+ * @author Mallory Henriet (311258)
+ * @author Louis Hervé (312937)
+ */
 public class PublicCardState {
     
     private final List<Card> faceUpCards;
     private final int deckSize, discardsSize;
 
+    /**
+     * Constructeur de PublicCardState.
+     *
+     * @param faceUpCards
+     *          les cartes face visible données
+     * @param deckSize
+     *          la taille de la pioche
+     * @param discardsSize
+     *          la taille de la défausse
+     * @throws IllegalArgumentException
+     *          si <code>faceUpCards</code> ne contient pas le bon nombre d'élément, <code>Constants.FACE_UP_CARDS_COUNT</code>
+     *          ou si la taille de la pioche est négative
+     *          ou si la taille de la défausse est négative
+     */
     public PublicCardState(List<Card> faceUpCards, int deckSize, int discardsSize) {
         Preconditions.checkArgument(
                 faceUpCards.size() == Constants.FACE_UP_CARDS_COUNT
@@ -23,28 +51,70 @@ public class PublicCardState {
         this.discardsSize = discardsSize;
     }
 
+    /**
+     * Retoune le nombre total de cartes qui ne sont pas en main des joueurs.
+     * C'est à dire les cartes face visible, de la pioche et celle de la défausse.
+     *
+     * @return
+     *          le nombre total de cartes qui ne sont pas en main des joueurs
+     */
     public int totalSize() {
         return faceUpCards.size() + deckSize + discardsSize;
     }
 
+    /**
+     * Retourne les 5 cartes face visible sous la forme d'une liste de 5 éléments.
+     *
+     * @return
+     *          retourne une liste de 5 élements formée des cartes face visible.
+     */
     public List<Card> faceUpCards() {
         return faceUpCards;
     }
 
+    /**
+     * Retourne la carte face visible d'index <code>slot</code>.
+     *
+     * @param slot
+     *          index de la carte des faces visibles à retourner
+     * @throws IndexOutOfBoundsException
+     *          si <code>slot</code> n'est pas compris entre 0 (inclus) et 5 (exclus)
+     * @return
+     *          la carte d'index <code>slot</code> des <code>faceUpCards</code>
+     */
     public Card faceUpCard(int slot) {
         return faceUpCards.get(
                 Objects.checkIndex(slot, Constants.FACE_UP_CARDS_COUNT)
         );
     }
-    
+
+    /**
+     * Retourne la taille de la pioche.
+     *
+     * @return
+     *          la taille de la pioche
+     */
     public int deckSize() {
         return deckSize;
     }
-    
+
+    /**
+     * Retourne vrai si et seulement si la pioche est vide.
+     *
+     * @return
+     *          vrai si la pioche est vide
+     *          faux dans le cas contraire
+     */
     public boolean isDeckEmpty() {
         return deckSize == 0;
     }
-    
+
+    /**
+     * Retourne la taille de la défausse.
+     *
+     * @return
+     *          la taille de la défausse
+     */
     public int discardsSize() {
         return discardsSize;
     }
