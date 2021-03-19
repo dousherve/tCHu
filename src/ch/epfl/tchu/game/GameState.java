74b94797781d8b3834ddff6c0990d7041124a286
Louis Dous Hervé <louisdousherve@gmail.com>
2021-03-19 18:31:45 +0100

    Début étape 5
    Signed-off-by: Louis Dous Hervé <louisdousherve@gmail.com>

---
+++ src/ch/epfl/tchu/game/GameState.java
@@ -0,0 +1,60 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.SortedBag;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
+
+public final class GameState extends PublicGameState {
+    
+    private final SortedBag<Ticket> tickets;
+    private final CardState cardState;
+    private final Map<PlayerId, PlayerState> playerState;
+    
+    private static Map<PlayerId, PublicPlayerState> makePublic(Map<PlayerId, PlayerState> playerState) {
+        EnumMap<PlayerId, PublicPlayerState> publicPlayerState = new EnumMap<>(PlayerId.class);
+        for (var entry : playerState.entrySet())
+            publicPlayerState.put(entry.getKey(), entry.getValue());
+        
+        return Collections.unmodifiableMap(publicPlayerState);
+    }
+    
+    public static GameState initial(SortedBag<Ticket> tickets, Random rng) {
+        final Deck<Card> ALL_CARDS = Deck.of(Constants.ALL_CARDS, rng); 
+        final Deck<Card> DECK_CARDS = ALL_CARDS.withoutTopCards(8);
+        
+        final PlayerId FIRST_PLAYER_ID = PlayerId.ALL.get(rng.nextInt(PlayerId.COUNT));
+        final PlayerId SECOND_PLAYER_ID = FIRST_PLAYER_ID.next();
+        
+        final PlayerState FIRST_PLAYER_STATE = PlayerState.initial(ALL_CARDS.topCards(4));
+        final PlayerState SECOND_PLAYER_STATE = PlayerState.initial(ALL_CARDS.withoutTopCards(4).topCards(4));
+        
+        final Map<PlayerId, PlayerState> playerState = Map.of(
+                FIRST_PLAYER_ID, FIRST_PLAYER_STATE,
+                SECOND_PLAYER_ID, SECOND_PLAYER_STATE
+        );
+        
+        return new GameState(tickets, CardState.of(DECK_CARDS), FIRST_PLAYER_ID, playerState);
+    }
+    
+    private GameState(SortedBag<Ticket> tickets, CardState cardState, PlayerId currentPlayerId, Map<PlayerId, PlayerState> playerState) {
+        super(tickets.size(), cardState, currentPlayerId, makePublic(playerState), null);
+        
+        this.tickets = tickets;
+        this.cardState = cardState;
+        this.playerState = playerState;
+    }
+    
+    @Override
+    public PlayerState playerState(PlayerId playerId) {
+        return playerState.get(playerId);
+    }
+    
+    @Override
+    public PlayerState currentPlayerState() {
+        return playerState(currentPlayerId());
+    }
+    
+}
---
+++ src/ch/epfl/tchu/game/PublicGameState.java
@@ -0,0 +1,68 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class PublicGameState {
+    
+    private final int ticketsCount;
+    private final PublicCardState cardState;
+    private final PlayerId currentPlayerId, lastPlayer;
+    private final Map<PlayerId, PublicPlayerState> playerState;
+    
+    public PublicGameState(int ticketsCount, PublicCardState cardState, PlayerId currentPlayerId, Map<PlayerId, PublicPlayerState> playerState, PlayerId lastPlayer) {
+        Preconditions.checkArgument(ticketsCount >= 0 && playerState.size() == 2);
+        
+        this.ticketsCount = ticketsCount;
+        this.cardState = Objects.requireNonNull(cardState);
+        this.currentPlayerId = Objects.requireNonNull(currentPlayerId);
+        this.playerState = Collections.unmodifiableMap(Objects.requireNonNull(playerState));
+        this.lastPlayer = lastPlayer;
+    }
+    
+    public int ticketsCount() {
+        return ticketsCount;
+    }
+    
+    public boolean canDrawTickets() {
+        return ticketsCount > 0;
+    }
+    
+    public PublicCardState cardState() {
+        return cardState;
+    }
+    
+    public boolean canDrawCards() {
+        return (cardState.deckSize() + cardState().discardsSize()) >= 5;
+    }
+    
+    public PlayerId currentPlayerId() {
+        return currentPlayerId;
+    }
+    
+    public PublicPlayerState playerState(PlayerId playerId) {
+        return playerState.get(playerId);
+    }
+    
+    public PublicPlayerState currentPlayerState() {
+        return playerState(currentPlayerId);
+    }
+    
+    public List<Route> claimedRoutes() {
+        List<Route> claimedRoutes = new ArrayList<>();
+        for (PublicPlayerState state : playerState.values())
+            claimedRoutes.addAll(state.routes());
+        
+        return Collections.unmodifiableList(claimedRoutes);
+    }
+    
+    public PlayerId lastPlayer() {
+        return lastPlayer;
+    }
+    
+}
