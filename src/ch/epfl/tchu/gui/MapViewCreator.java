109a6f78e5e995a8635de832839eeb7123bc0d4d
MalloryHenriet <henrietma@gmail.com>
2021-04-30 15:09:57 +0200

    Début Etape 9

---
+++ src/ch/epfl/tchu/gui/MapViewCreator.java
@@ -0,0 +1,23 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.Card;
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.image.ImageView;
+
+import java.util.List;
+
+import static ch.epfl.tchu.gui.ActionHandlers.ChooseCardsHandler;
+import static ch.epfl.tchu.gui.ActionHandlers.ClaimRouteHandler;
+
+final class MapViewCreator {
+
+    public ImageView createMapView(ObservableGameState ogs, ObjectProperty<ClaimRouteHandler> handler, CardChooser cardChooser) {
+        return new ImageView("map.png");
+    }
+
+    @FunctionalInterface
+    interface CardChooser {
+        void chooseCards(List<SortedBag<Card>> options, ChooseCardsHandler handler);
+    }
+}
---
+++ src/ch/epfl/tchu/gui/ObservableGameState.java
@@ -0,0 +1,16 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.game.PlayerId;
+import ch.epfl.tchu.game.PlayerState;
+import ch.epfl.tchu.game.PublicGameState;
+
+public final class ObservableGameState {
+
+    private PublicGameState gameState;
+    private PlayerState playerState;
+    //private final PlayerId playerId;
+
+    public ObservableGameState(PlayerId playerId) {
+
+    }
+}
